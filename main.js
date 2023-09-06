1c30a73516c9e91fc294af16cb2721f3456e7339
Mizu <124433727+MIzuw@users.noreply.github.com>
2023-09-06 11:39:39 +0200

    coloring works now (only on the Y axis, as the X axis isn't created yet)

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-const DEFAULT_SIZE = 16
-let size = DEFAULT_SIZE
+const DEFAULT_SIZE = 2;
+const DEFAULT_COLOR = '#000000';
+
+let size = DEFAULT_SIZE;
+currentColor = DEFAULT_COLOR;
 
 const grid = document.getElementById("grid");
 
 function createGrid() {
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement('div');
-        gridElement.classList.add('grid-element');
+        gridElement.classList.add('GridElement');
+        gridElement.addEventListener("mouseenter", function(e) {
+            e.target.style.backgroundColor = currentColor
+            console.log("Test");
+        })
         grid.appendChild(gridElement);
     }
+}
+
+function clearGrid() {
+    grid.innerHTML = "";
+    createGrid();
 }
