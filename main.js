4e7fb2750c34acd2eb5681adceffcb76a6d5adfd
Mizu <124433727+Mizuw@users.noreply.github.com>
2023-09-06 22:51:39 +0200

    code commenting

--- main.js
+++ main.js
@@ -4,24 +4,26 @@ const ParaSlider = document.getElementById("ParaSlider");
 const UserColor = document.getElementById("UserColor");
 
 let currentMode = "colorMode";
-let size = slider.value;
+let size = slider.value; // the size of the grid is defined by the slider
 
-slider.oninput = resizeGrid;
+slider.oninput = resizeGrid; // the grid will reload after the slider moves
 ParaSlider.textContent = slider.value + " x " + slider.value;
 
 function createGrid() {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)` // this code makes it 
     grid.style.gridTemplateRows =  `repeat(${size}, 1fr)`
 
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('GridElement');
+
         gridElement.addEventListener("mouseenter", function(e) {
-            if (currentMode == "rainbowMode") {
+            if (currentMode == "rainbowMode") { // we generate three random numbers, so we can set them into RGB. This is then used by the rainbow mode
                 const RGBred = Math.floor(Math.random() * 255) + 1; 
                 const RGBgreen = Math.floor(Math.random() * 255) + 1; 
                 const RGByellow = Math.floor(Math.random() * 255) + 1; 
                 e.target.style.backgroundColor = `rgb(${RGBred}, ${RGBgreen}, ${RGByellow})`;
+
             } else if (currentMode === "colorMode") {
                 e.target.style.backgroundColor = UserColor.value;
             } else if (currentMode === "eraserMode") {
@@ -40,13 +42,12 @@ function resizeGrid() {
 
 function clearGrid() {
     grid.innerHTML = "";
-    createGrid();
+    createGrid(); 
 }
 
 function selectMode() {
     if (this.document.activeElement.getAttribute("id") == "rainbowMode" || "eraserMode" || "colorMode") {
-        currentMode = this.document.activeElement.getAttribute("id");
+        currentMode = this.document.activeElement.getAttribute("id"); // gets the ID of the buttons
     }
 }
-
-window.onload = createGrid()
+window.onload = createGrid() // creates a grid after the page was loaded
